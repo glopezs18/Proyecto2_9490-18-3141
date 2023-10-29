986c520b2530930050491c9066aba6c8b2c126f2
glopezs18 <glopezs18@miumg.edu.gt>
2023-10-28 21:04:46 -0600

    update component profile

--- src/components/Productos.tsx
+++ src/components/Productos.tsx
@@ -1,11 +1,15 @@
+import {CSSProperties} from "react" ;
 import EnvConfig from '../env/EnvConfig'
-import RestrictedCartBtnUser from '../auth/RestrictedCartBtnUser';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/productos.scss'
 
 function Productos(props: any) {
   // const IMAGE_URL = "http://localhost:3000/images/";
   const IMAGE_URL = EnvConfig().IMAGE_URL;
+  const token = localStorage.getItem("auth");
+  const btn_style: CSSProperties = {
+    display: token ?  "block" : "none"
+  }
   return (
     <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={props._id} data-id={props._id}>
       <div className="card" style={{minHeight: "304px", padding: "20px"}}>
@@ -13,10 +17,9 @@ function Productos(props: any) {
         <div className="card-body">
           <h5 className="card-title">{props.nombre}</h5>
           <p className="card-text">Q{props.precio}</p>
-          <RestrictedCartBtnUser props={props} />
-          {/* <button className="btn btn-primary" onClick={props.onAddToCart}>
+          <button style={btn_style} className="btn btn-primary" onClick={props.onAddToCart}>
             Agregar al carrito
-          </button> */}
+          </button>
         </div>
       </div>
     </div>
